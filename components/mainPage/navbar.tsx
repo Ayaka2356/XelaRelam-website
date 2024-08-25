8b627238919a2e71fe601acfeb2fc7ee78994676
Dark-LYNN <botdevlynn@gmailcom>
2024-08-25 14:07:55 +0200

    Header Edit

--- components/mainPage/navbar.tsx
+++ components/mainPage/navbar.tsx
@@ -13,8 +13,10 @@ const Navbar = () => {
     <nav className={`${styles.header} p-4`}>
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          <img src="/assets/images/logo.png" alt="Logo" className="h-8 w-8 mr-2" />
-          <span className="text-white text-xl font-semibold">XelaRelam</span>
+          <Link href={"/"}>
+            <img src="/assets/images/logo.png" alt="Logo" className="h-8 w-8 mr-2" />
+            <span className="text-white text-xl font-semibold">XelaRelam</span>
+          </Link>
         </div>
         <div className="hidden md:flex space-x-4">
           <Link href="/">
